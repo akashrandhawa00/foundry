6d550d6cb94e5b16ce8aacc9a2603d266a8aecd4
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2026-06-29 00:56:12 -0400

    Add a role popup in production log

--- src/components/ui/ProductionRunRow.tsx
+++ src/components/ui/ProductionRunRow.tsx
@@ -20,6 +20,7 @@ function yeildRate(run: ProductionRun) {
 
 export const ProductionRunRow = ({ run }: { run: ProductionRun }) => {
     const [isExpanded, setIsExpanded] = useState(false);
+    const [showRole, setShowRole] = useState(false);
 
     const totalLoss = run.qtyDefects + run.qtyFallOff;
     const createdAt = new Date(run.createdAt).toLocaleString("en-CA", {
@@ -31,7 +32,7 @@ export const ProductionRunRow = ({ run }: { run: ProductionRun }) => {
         <>
             <tr
                 onClick={() => setIsExpanded((prev) => !prev)}
-                className={`${isExpanded ? "bg-surface-active" : ""} hover:bg-brand/40 cursor-pointer transition-colors duration-200 border-t border-surface-active `}
+                className={`${isExpanded ? "bg-surface-active/60 hover:bg-surface-active" : "hover:bg-brand/40"}  cursor-pointer transition-colors duration-200 border-t border-surface-active `}
             >
                 <td className={`${tdBaseStyle}`}>{run.id}</td>
                 <td className={`${tdBaseStyle}`}>{run.partNumber}</td>
@@ -56,9 +57,9 @@ export const ProductionRunRow = ({ run }: { run: ProductionRun }) => {
             {/* expanded row */}
             {isExpanded && (
                 <tr
-                    className={`${isExpanded ? "bg-surface-active" : ""} border-b border-white/20 animate-fadeIn`}
+                    className={`${isExpanded ? "bg-surface-active/60 hover:bg-surface-active" : ""} border-b border-white/20 animate-fadeIn`}
                 >
-                    <td colSpan={8}>
+                    <td colSpan={8} className="pt-3">
                         <div className="grid grid-cols-4 gap-3 mb-4 mx-4">
                             <div className={`${cardBaseStyle}`}>
                                 <p className={cardHeadingStyle}>Yield Rate</p>
@@ -82,10 +83,18 @@ export const ProductionRunRow = ({ run }: { run: ProductionRun }) => {
 
                         {/* additiona info  */}
                         <div className="text-sm gap-6 flex text-text-muted px-3 py-2">
-                            <span>
+                            <span className="group">
                                 Logged By:{" "}
-                                <span className="text-text-secondary">
+                                <span
+                                    className="text-text-secondary relative"
+                                    onClick={() => setShowRole((prev) => !prev)}
+                                >
                                     {run.loggedBy}
+                                    {showRole && (
+                                        <div className="translate-y-2 translate-x-5 opacity-0 transition-all duration-200 delay-500 group-hover:opacity-100 text-white group-hover:translate-y-1 px-2 py-2 rounded bg-brand/90 border border-brand absolute">
+                                            Supervisor
+                                        </div>
+                                    )}
                                 </span>
                             </span>
                             <span>
